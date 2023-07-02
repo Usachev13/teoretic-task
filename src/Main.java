d13310ea693f998d701175e90f03b160b7b18439
Usachev13 <2893318@gmail.com>
2023-07-02 20:18:17 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,23 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        int [] array = new int[7];
+        for (int i = 0; i < array.length; i++){
+            array[i] = i + 1;
+//            System.out.println(array[i]);
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0){
+                array[i] += 1;
+            }
+//            System.out.println(array[i]);
+
+        }
+        int [] temp = Arrays.copyOfRange(array, 2, 6);
+        for (int j : temp) {
+            System.out.println(j);
+
+        }
 
 
 
