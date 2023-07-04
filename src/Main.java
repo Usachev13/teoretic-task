3df4b8d50ba5f66895b16072f7f7a158eee33538
Usachev13 <2893318@gmail.com>
2023-07-04 19:13:59 +0300

    errors done

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,25 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int [] array = new int[7];
-        for (int i = 0; i < array.length; i++){
-            array[i] = i + 1;
-//            System.out.println(array[i]);
-        }
+        int[] array = new int[7];
         for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(createNewArray(array)));
+    }
+    public static int [] createNewArray(int [] array){
+        int [] newArray = new int[5];
+        int j = 0;
+        for (int i = 0; i < array.length; i++){
             if (array[i] % 2 == 0){
                 array[i] += 1;
             }
-//            System.out.println(array[i]);
-
+            if (i >= 2){
+                newArray[j] = array[i];
+                j++;
+            }
         }
-        int [] temp = Arrays.copyOfRange(array, 2, 6);
-        for (int j : temp) {
-            System.out.println(j);
-
-        }
-
-
-
-
-
-
+        return newArray;
     }
 
 }
